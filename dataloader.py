refactor code to run without tiling, improved visualization, removed duplicate get_precision and get_recall in evaluator.py, fixed issue with precision and recall calculation

dataloader.py
@@ -12,8 +12,8 @@ from PIL import Image
 
 # img_dir is the directory where the images are located
 # please modify as needed to match the folder structure
-img_dir = './training_data/color/'
-mask_dir = './training_data/mask/'
+img_dir = '../training_data/color/'
+mask_dir = '../training_data/mask/'
 
 data_filenames = sorted(os.listdir(img_dir))
 mask_filenames = sorted(os.listdir(mask_dir))
@@ -71,6 +71,12 @@ def threshold_mask(img):
         pic, 1, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
     return threshed
 
+# Reference: https://discuss.pytorch.org/t/simple-way-to-inverse-transform-normalization/4821
+# utility function to reverse normalization (multiply by standard dev and add back mean)
+def unnormalize(tensor, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+    for t, m, s in zip(tensor, mean, std):
+        t.mul_(s).add_(m)
+    return tensor
 
 def main(batch_size=8, num_workers=2):
     # Resize and CenterCrop accepts either PIL Image or Tensor.
@@ -115,21 +121,21 @@ def main(batch_size=8, num_workers=2):
     loader_test = DataLoader(
         data_test, batch_size=batch_size, shuffle=False, num_workers=0, drop_last=True)
 
+    print(data_train[0][0].shape, data_train[0][1].shape)
+    
     figure = plt.figure(figsize=(12, 6))
     cols, rows = 3, 2
     for i in range(1, int((cols * rows)/2 + 1)):
         sample_idx = torch.randint(len(data_train), size=(1,)).item()
-        img, mask, img_path, mask_path = data_train[sample_idx]
-        print(img_path)
-        print(mask_path)
+        img, mask = data_train[sample_idx]
         figure.add_subplot(rows, cols, i)
         plt.title(f"Image {i}")
         plt.axis("off")
-        plt.imshow(img.permute(1, 2, 0))
+        plt.imshow(unnormalize(img).permute(1, 2, 0))
         j = cols + i
         figure.add_subplot(rows, cols, j)
         plt.title(f"Image {i}")
         plt.axis("off")
-        plt.imshow(mask.permute(1, 2, 0))
+        plt.imshow((mask.permute(1, 2, 0)*255))
     plt.show()
     return loader_train, loader_val, loader_test

evaluator.py
@@ -1,19 +1,9 @@
 import torch
 import numpy as np
 import gc
+import trainer
 
-
-def get_acc(predictions, labels):
-    return torch.sum(predictions == labels)/(labels.shape[0]*labels.shape[1])
-
-
-def get_recall(predictions, labels):
-    return torch.sum((predictions == labels) * (labels == 1))/torch.sum(labels == 1)
-
-
-def get_precision(predictions, labels):
-    return torch.sum((predictions == labels) * (labels == 1))/torch.sum(predictions == 1)
-
+# forward pass for inference and evaluation
 def get_inference_output(model, X_in, device):
     model.eval()
     X_in = X_in.to(device)
@@ -31,11 +21,10 @@ def evaluate_model(model, loader_val, loss_fn, device):
     # set model to eval mode when evaluating on validation set
     model.eval()
     for X_val, y_val in loader_val:
-        # combine the batch and tile dimension
-        X_val = X_val.reshape(-1, X_val.shape[1], X_val.shape[3], X_val.shape[4])
-        # add a dimension to match prediction shape
         # only take the first color channel of mask
-        y_val = y_val[:,0,:,:,:].reshape(-1, y_val.shape[-2] * y_val.shape[-1])
+        y_val = y_val[:, 0, :, :]
+        # flatten the mask image
+        y_val = y_val.reshape(-1, y_val.shape[-2] * y_val.shape[-1])
         X_val, y_val = X_val.to(device), y_val.to(device)
         
         with torch.no_grad():
@@ -45,9 +34,9 @@ def evaluate_model(model, loader_val, loss_fn, device):
             predictions = torch.where(output > 0, 1, 0)
             batch_loss = loss_fn(output, y_val.float())
             
-        batch_acc = get_acc(predictions, y_val)
-        batch_recall = get_recall(predictions, y_val)
-        batch_precision = get_precision(predictions, y_val)
+        batch_acc = trainer.get_acc(predictions, y_val)
+        batch_recall = trainer.get_recall(predictions, y_val)
+        batch_precision = trainer.get_precision(predictions, y_val)
         
         acc_val.append(batch_acc.item())
         loss_val.append(batch_loss.item())

trainer.py
@@ -2,25 +2,29 @@ import torch
 import numpy as np
 import os
 import gc
-
+import torch.nn as nn
 import evaluator
 
 
 def get_acc(predictions, labels):
+    # labels.shape[0]*labels.shape[1] is height * width, since we are doing pixel-level classification
     return torch.sum(predictions == labels)/(labels.shape[0]*labels.shape[1])
 
 
 def get_recall(predictions, labels):
-    return torch.sum((predictions == labels) * (labels == 1))/torch.sum(labels == 1)
+    # True Positive / all positive
+    return torch.sum((predictions == labels) * (labels == 0))/torch.sum(labels == 0)
 
 
 def get_precision(predictions, labels):
-    return torch.sum((predictions == labels) * (labels == 1))/torch.sum(predictions == 1)
+    # True Positive / preditcted positive
+    return torch.sum((predictions == labels) * (labels == 0))/torch.sum(predictions == 0)
+
 
 # This function is used to train the model
 # Reference: pytorch official tutorial
 # https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
-def train(model, loader_train, loader_val, lr=1e-4, num_epochs=10, device='cpu', patience=5, evaluation_interval=200):
+def train(model, loader_train, loader_val, lr=1e-4, num_epochs=10, device='cpu', patience=5, evaluation_interval=None, pos_weight=None):
     # initialize lists to store logs of the validation loss and validation accuracy
     val_loss_hist = []
     val_acc_hist = []
@@ -29,10 +33,9 @@ def train(model, loader_train, loader_val, lr=1e-4, num_epochs=10, device='cpu',
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     # From documentation: This loss combines a Sigmoid layer and the BCELoss in one single class.
-    weight = False
-    if weight:
+    if pos_weight:
         loss_fn = nn.BCEWithLogitsLoss(
-            pos_weight=torch.Tensor(weight).to(device))
+            pos_weight=torch.Tensor(pos_weight).to(device))
     else:
         loss_fn = nn.BCEWithLogitsLoss()
 
@@ -55,13 +58,11 @@ def train(model, loader_train, loader_val, lr=1e-4, num_epochs=10, device='cpu',
 
         # Training pass
         for X_batch, y_batch in loader_train:
-            # combine the batch and tile dimension
-            X_batch = X_batch.reshape(-1,
-                                      X_batch.shape[1], X_batch.shape[3], X_batch.shape[4])
-            # add a dimension to match prediction shape
+            
             # only take the first color channel of mask
-            y_batch = y_batch[:, 0, :, :,
-                              :].reshape(-1, y_batch.shape[-2] * y_batch.shape[-1])
+            y_batch = y_batch[:, 0, :, :]
+            # flatten the mask image
+            y_batch = y_batch.reshape(-1, y_batch.shape[-2] * y_batch.shape[-1])
 
             # torch tensor can be loaded to GPU, when applicable
             X_batch, y_batch = X_batch.float().to(device), y_batch.to(device)

visualizer.py
@@ -1,48 +1,41 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-
 import trainer
 import evaluator
-
-TILE_HEIGHT = 80
-TILE_WIDTH = 80
-
-def plot_tiles(img, num_tiles_v, num_tiles_h, channel=None):
-    if channel != None:
-        img = img[channel]      
-    v, h = num_tiles_v, num_tiles_h 
-    if v == h == 1:
-        print(img.shape)
-        plt.imshow(np.absolute(img.squeeze()))
-        plt.axis('off')
-        plt.show()
-    else:
-        fig, axs = plt.subplots(v, h) 
-        for i in range(v):
-            for j in range(h):
-                # absolute is used because plt.imshow has odd habit of showing -0.0 as bright blocks
-                axs[i,j].imshow(np.absolute(img[i*h+j]))
-                axs[i,j].axis('off')
-        plt.subplots_adjust(wspace=0.05, hspace=0.05)
-
-def visualize_model_output(dataloader, model, num, tile_height=TILE_HEIGHT, tile_width=TILE_WIDTH, device='cuda'):
-    while num > 0:
-        x, y = next(iter(dataloader))
-        print(x.shape, y.shape)
-        batchsize = x.shape[0]
-        x_out = evaluator.get_inference_output(model, x.reshape(-1, x.shape[1], x.shape[3], x.shape[4]), device)
-        x_out = torch.where(x_out > 0, 1, 0).cpu()
-        y_reshape = y[:,0,:,:,:].reshape(-1, y.shape[-2] * y.shape[-1]).cpu()
-        print(f"Accuracy = {trainer.get_acc(x_out, y_reshape).item() * 100}%")
-        print(f"Precision = {trainer.get_precision(x_out, y_reshape).item() * 100}%")
-        print(f"Recall = {trainer.get_recall(x_out, y_reshape).item() * 100}%")
-        x_out_reshaped = x_out.reshape(-1, y.shape[2], y.shape[3], y.shape[4]).cpu()
-
-        v, h = int(480/tile_height), int(640/tile_width)
-        for i in range(min(num, batchsize)):
-            plot_tiles(x[i], v, h, 0)
-            plot_tiles(x_out_reshaped[i], v, h)
-            plot_tiles(y[i], v, h, 0)
-
-        num -= min(num, batchsize)
+from dataloader import unnormalize
+        
+def visualize_model_output(dataloader, model, device='cuda'):        
+    cols, rows = 5, 3
+    figure = plt.figure(figsize=(cols*3, rows*3))
+    
+    # evaluate a single batch and reshape for display
+    x, y = next(iter(dataloader))
+    batchsize = x.shape[0]
+    x_out = evaluator.get_inference_output(model, x, device)
+    x_out = torch.where(x_out > 0, 1, 0).cpu()
+    # flatten y for computing evaluation metrics
+    y_flatten = y[:,0,:,:].reshape(-1, y.shape[-2] * y.shape[-1]).cpu()
+    print(f"Accuracy = {trainer.get_acc(x_out, y_flatten).item() * 100}%")
+    print(f"Precision = {trainer.get_precision(x_out, y_flatten).item() * 100}%")
+    print(f"Recall = {trainer.get_recall(x_out, y_flatten).item() * 100}%")
+    # reshape model output to have shape (batch_size, channel, height, width)
+    x_out = x_out.reshape(-1, y.shape[1], y.shape[2], y.shape[3]).cpu()
+    for i in range(1, cols+1):
+        sample_idx = torch.randint(x.shape[0], size=(1,)).item()
+        img, prediction, mask = x[i], x_out[i], y[i]
+        figure.add_subplot(rows, cols, i)
+        plt.title(f"Image {i}")
+        plt.axis("off")
+        plt.imshow(unnormalize(img).permute(1, 2, 0))
+        j = cols + i
+        figure.add_subplot(rows, cols, j)
+        plt.title(f"Predicted {i}")
+        plt.axis("off")
+        k = cols * 2 + i
+        plt.imshow(prediction.permute(1, 2, 0))
+        figure.add_subplot(rows, cols, k)
+        plt.title(f"Actual {i}")
+        plt.axis("off")
+        plt.imshow(mask.permute(1, 2, 0))
+    plt.show()