ac6fbd47fa1b211bf738df1bc623de1558407aea
Ethan Ruoff <ethanruoff@gmail.com>
2021-12-07 13:16:18 -0500

    Trained MSE model

--- visualizer.py
+++ visualizer.py
@@ -2,8 +2,11 @@ import torch
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2 as cv
 import trainer
+import finger_trainer
 import evaluator
+import finger_evaluator
 from dataloader import unnormalize
 from torchvision import transforms
 from PIL import Image
@@ -41,6 +44,44 @@ def visualize_model_output(dataloader, model, device='cuda'):
         plt.title(f"Actual {i}")
         plt.axis("off")
         plt.imshow(mask.permute(1, 2, 0))
+    plt.show()
+
+def visualize_finger_model_output(dataloader, model, device='cuda'):        
+    cols, rows = 8, 3
+    figure = plt.figure(figsize=(cols*3, rows*3))
+    
+    # evaluate a single batch and reshape for display
+    x, y = next(iter(dataloader))
+    batchsize = x.shape[0]
+    x_out = evaluator.get_inference_output(model, x, device)
+    #x_out = torch.where(x_out > 0, 1, 0).cpu()
+    x_out = x_out.cpu()
+    print(f"Accuracy = {finger_trainer.get_acc_fingertip(x_out, y).item() * 100}%")
+    # reshape model output to have shape (batch_size, channel, height, width)
+    #x_out = x_out.reshape(-1, y.shape[1], y.shape[2], y.shape[3]).cpu()
+    for i in range(1, cols+1):
+        sample_idx = torch.randint(x.shape[0], size=(1,)).item()
+        img, finger_prediction, finger_label = x[i], x_out[i], y[i]
+        img = unnormalize(img).cpu().permute(1, 2, 0).numpy().copy()
+        figure.add_subplot(rows, cols, i)
+        plt.title(f"Image {i}")
+        plt.axis("off")
+        plt.imshow(img)
+        j = cols + i
+        figure.add_subplot(rows, cols, j)
+        finger_prediction = np.rint((finger_prediction[0].item(), finger_prediction[1].item()))
+        finger_prediction = (int(finger_prediction[0]), int(finger_prediction[1]))
+        plt.title(f"Predicted {i} Coor {finger_prediction}")
+        plt.axis("off")
+        prediction_image = cv.circle(img, finger_prediction, 7, (255,0,0), 1)
+        plt.imshow(prediction_image)
+        k = cols * 2 + i
+        figure.add_subplot(rows, cols, k)
+        finger_label = (finger_label[0].item(), finger_label[1].item())
+        plt.title(f"Actual {i} Coor {finger_label}")
+        plt.axis("off")
+        label_image = cv.circle(img, finger_label, 7, (255,0,0), 1)
+        plt.imshow(label_image)
     plt.show()
     
     
